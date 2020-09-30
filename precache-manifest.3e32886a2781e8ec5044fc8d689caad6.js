808e49695ec695e6808fe08586dd0666f48159e1
leni lunderman <lenilunderman@gmail.com>
2020-09-30 10:15:57 -0400

    Updates

---
+++ precache-manifest.3e32886a2781e8ec5044fc8d689caad6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa2516f4c6fde89ca143dc392c17ec9",
+    "url": "/blogpost-search/index.html"
+  },
+  {
+    "revision": "2f622756d1c9b3c2f64a",
+    "url": "/blogpost-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c524064805c095ef18c5",
+    "url": "/blogpost-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2f622756d1c9b3c2f64a",
+    "url": "/blogpost-search/static/js/2.8d40338c.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/blogpost-search/static/js/2.8d40338c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c524064805c095ef18c5",
+    "url": "/blogpost-search/static/js/main.6c06603c.chunk.js"
+  },
+  {
+    "revision": "ed915126851a2169903e",
+    "url": "/blogpost-search/static/js/runtime-main.be98b178.js"
+  }
+]);
